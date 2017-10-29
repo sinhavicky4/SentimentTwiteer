456ec17468e15a06adfcf33cdc7c1b204a7ae132
Priyanshu Singh <sinhavicky4@gmail.com>
2017-10-30 03:08:49 +0530

    Commit

--- src/main/scala/com/SentimentTwiteer.scala
+++ src/main/scala/com/SentimentTwiteer.scala
@@ -11,9 +11,6 @@ object SentimentTwiteer {
   def main(args: Array[String]): Unit = {
 
    LoggerStreaming.setLoggerLevel()
-
-
-
     if(args.length<4){
       System.out.print("Enter Consumer Key (API Key) Consumer Secret (API Secret)Access Token Access Token Secret")
       System.exit(1);
@@ -21,7 +18,7 @@ object SentimentTwiteer {
 
 
     val Array(customer_key,customer_secrect,access_token,access_token_secret)=args.take(4)
-    val filters=args.takeRight(args.length-4)
+    val favTag=args.takeRight(args.length-4)
 
     System.setProperty("twitter4j.oauth.consumerKey",customer_key)
     System.setProperty("twitter4j.oauth.consumerSecret",customer_secrect)
@@ -30,20 +27,20 @@ object SentimentTwiteer {
 
 
 
-
+    //configuration
     val conf=new SparkConf().setAppName("Sentiment").setMaster("local[5]")
-
+    //everything begin here.getting StreamContext.Batch of 2 sec
     val scc=new StreamingContext(conf,Seconds(2))
 
     //Dstream
-    val stream=TwitterUtils.createStream(scc,None,filters)
+    val dStream=TwitterUtils.createStream(scc,None,favTag)
 
-    val hashTag=stream.flatMap(status=>{status.getText.split(" ").filter(_.startsWith("#"))})
+    val hashTag=dStream.flatMap(status=>{status.getText.split(" ").filter(_.startsWith("#"))})
 
     val topHashTag60=hashTag.map((_,1)).reduceByKeyAndWindow(_+_,Seconds(60))
       .map{case (topic,count)=>(topic,count)}.transform(_.sortByKey(false))
 
-    val topHashTag10=hashTag.map((_,1)).reduceByKeyAndWindow(_+_,Seconds(10))
+    val topHashTag120=hashTag.map((_,1)).reduceByKeyAndWindow(_+_,Seconds(120))
       .map{case (topic,count)=>(topic,count)}.transform(_.sortByKey(false)).saveAsTextFiles("/root/Desktop/outtweet")
 
 
@@ -54,7 +51,8 @@ object SentimentTwiteer {
 
     })
 
-   /* topHashTag10.foreachRDD(rdd=>{
+
+ /*  topHashTag120.foreachRDD(rdd=>{
       val topList=rdd.take(10)
       println("Popular topic in last 10 sec (%s total)".format(rdd.count()))
       topList.foreach{case (count,tag)=>println("%s (%s tweets)".format(tag,count))}
