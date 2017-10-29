4df75ca6da896a8966e0309cc08d4058439da8d0
Priyanshu Singh <sinhavicky4@gmail.com>
2017-10-30 03:12:55 +0530

    Commit

--- src/main/scala/com/LoggerStreaming.scala
+++ src/main/scala/com/LoggerStreaming.scala
@@ -1,25 +1,16 @@
 package com
 
-
-
-
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.Logging
 
-
-
-
 object LoggerStreaming extends Logging {
 
-  def setLoggerLevel(): Unit ={
+  def setLoggerLevel(): Unit = {
 
-    val v=Logger.getRootLogger.getAllAppenders.hasMoreElements
-    if(!v){
+    val v = Logger.getRootLogger.getAllAppenders.hasMoreElements
+    if (!v) {
       Logger.getRootLogger.setLevel(Level.WARN)
     }
-
-
-
 
   }
 
--- src/main/scala/com/SentimentTwiteer.scala
+++ src/main/scala/com/SentimentTwiteer.scala
@@ -1,62 +1,61 @@
 package com
 
-import org.apache.spark.{SparkConf}
+import org.apache.spark.SparkConf
 import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-
-
 
 
 object SentimentTwiteer {
   def main(args: Array[String]): Unit = {
 
-   LoggerStreaming.setLoggerLevel()
-    if(args.length<4){
+    LoggerStreaming.setLoggerLevel()
+    if (args.length < 4) {
       System.out.print("Enter Consumer Key (API Key) Consumer Secret (API Secret)Access Token Access Token Secret")
       System.exit(1);
     }
 
 
-    val Array(customer_key,customer_secrect,access_token,access_token_secret)=args.take(4)
-    val favTag=args.takeRight(args.length-4)
+    val Array(customer_key, customer_secrect, access_token, access_token_secret) = args.take(4)
+    val favTag = args.takeRight(args.length - 4)
 
-    System.setProperty("twitter4j.oauth.consumerKey",customer_key)
-    System.setProperty("twitter4j.oauth.consumerSecret",customer_secrect)
-    System.setProperty("twitter4j.oauth.accessToken",access_token)
-    System.setProperty("twitter4j.oauth.accessTokenSecret",access_token_secret)
-
+    System.setProperty("twitter4j.oauth.consumerKey", customer_key)
+    System.setProperty("twitter4j.oauth.consumerSecret", customer_secrect)
+    System.setProperty("twitter4j.oauth.accessToken", access_token)
+    System.setProperty("twitter4j.oauth.accessTokenSecret", access_token_secret)
 
 
     //configuration
-    val conf=new SparkConf().setAppName("Sentiment").setMaster("local[5]")
+    val conf = new SparkConf().setAppName("Sentiment").setMaster("local[5]")
     //everything begin here.getting StreamContext.Batch of 2 sec
-    val scc=new StreamingContext(conf,Seconds(2))
+    val scc = new StreamingContext(conf, Seconds(2))
 
     //Dstream
-    val dStream=TwitterUtils.createStream(scc,None,favTag)
+    val dStream = TwitterUtils.createStream(scc, None, favTag)
 
-    val hashTag=dStream.flatMap(status=>{status.getText.split(" ").filter(_.startsWith("#"))})
+    val hashTag = dStream.flatMap(status => {
+      status.getText.split(" ").filter(_.startsWith("#"))
+    })
 
-    val topHashTag60=hashTag.map((_,1)).reduceByKeyAndWindow(_+_,Seconds(60))
-      .map{case (topic,count)=>(topic,count)}.transform(_.sortByKey(false))
+    val topHashTag60 = hashTag.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+      .map { case (topic, count) => (topic, count) }.transform(_.sortByKey(false))
 
-    val topHashTag120=hashTag.map((_,1)).reduceByKeyAndWindow(_+_,Seconds(120))
-      .map{case (topic,count)=>(topic,count)}.transform(_.sortByKey(false)).saveAsTextFiles("/root/Desktop/outtweet")
+    val topHashTag120 = hashTag.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(120))
+      .map { case (topic, count) => (topic, count) }.transform(_.sortByKey(false)).saveAsTextFiles("/root/Desktop/outtweet")
 
 
-    topHashTag60.foreachRDD(rdd=>{
-      val topList=rdd.take(10)
+    topHashTag60.foreachRDD(rdd => {
+      val topList = rdd.take(10)
       println("Popular topic in last 60 sec (%s total)".format(rdd.count()))
-      topList.foreach{case (count,tag)=>println("%s (%s tweets)".format(tag,count))}
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
 
     })
 
 
- /*  topHashTag120.foreachRDD(rdd=>{
-      val topList=rdd.take(10)
-      println("Popular topic in last 10 sec (%s total)".format(rdd.count()))
-      topList.foreach{case (count,tag)=>println("%s (%s tweets)".format(tag,count))}
-    })*/
+    /*  topHashTag120.foreachRDD(rdd=>{
+         val topList=rdd.take(10)
+         println("Popular topic in last 10 sec (%s total)".format(rdd.count()))
+         topList.foreach{case (count,tag)=>println("%s (%s tweets)".format(tag,count))}
+       })*/
     //start streaming
     scc.start()
     scc.awaitTermination()
